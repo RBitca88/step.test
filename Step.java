1b136a4bc8b6bde22a8430f3485793e24a62039f
RBitca88 <radubitca4@gmail.com>
2021-11-18 19:39:05 +0200

    Changed the displayed messages in main().

--- Step.java
+++ Step.java
@@ -2,6 +2,7 @@ public class Step {
 	
 	public static void main(String[] args){
 		
-		System.out.println("Hello World!");
+		System.out.println("Hello Step!");
+		System.out.println("Test String!");
 	}
 }
---
+++ Step.java.bak
@@ -0,0 +1,7 @@
+public class Step {
+	
+	public static void main(String[] args){
+		
+		System.out.println("Hello World!");
+	}
+}
